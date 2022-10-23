ff7fd92507c4465acde0ee0259edc9f976eadbcd
jioniy <jw010208@gmail.com>
2022-10-23 20:09:04 +0900

    [ADD] #2 H2 Console 결과 확인을 위한 Security 설정

--- jwt-tutorial/src/main/java/me/jioniy/jwttutorial/config/SecurityConfig.java
+++ jwt-tutorial/src/main/java/me/jioniy/jwttutorial/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package me.jioniy.jwttutorial.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -10,6 +11,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 * */
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers(
+                        "/h2-console/**"
+                        ,"/favicon.ico"
+                );
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
